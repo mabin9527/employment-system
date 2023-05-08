8d26ee0bc86b115dbb4283f925ded568969ca33e
mabin9527 <ma.bin9527000@gmail.com>
2023-05-08 19:42:27 +0000

    Add code to allow front end operation to get all the deparment list from database

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from my_app import models
 
 
 def depart_list(request):
     """
     Department list
     """
-    return render(request, 'depart_list.html')
+
+    # Get all the deparment list from database
+    queryset = models.Department.objects.all()
+    return render(request, 'depart_list.html', queryset)
