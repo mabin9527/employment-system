72c37ca3b1ad661081078c2c067131ad9123ea61
mabin9527 <ma.bin9527000@gmail.com>
2023-05-09 10:24:14 +0000

    Adjust code to models.py to allow user to add department data on department database

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from my_app import models
-from .forms import DepartmentForm
+from django import forms
 
 
 def depart_list(request):
@@ -12,22 +12,31 @@ def depart_list(request):
     queryset = models.Department.objects.all()
     return render(request, 'depart_list.html', {'queryset': queryset})
 
+class DepartmentForm(forms.ModelForm):
+    """
+    Create form class to allow user to add department data
+    """
+    class Meta:
+        model = models.Department
+        fields = ['title',]
+        widgets = {
+            'title': forms.TextInput(attrs= {'class': 'form-control'}),
+        }
+
 def depart_add(request):
     """
     First to check if the request.method is GET method. If it is true, then link the user to 
     depart_add page. If the request.method is POST method, save the data collected from user
     to database and redirect user to depart_list page 
     """
-    if request.method == 'POST':
-        form = DepartmentForm(request.POST)
-        if form.is_valid():
-            return redirect('/depart/list')
+    if request.method == 'GET':
+        form = DepartmentForm()
+        return render(request, 'depart_add.html', {'form': form})
 
-    form = DepartmentForm()       
-    context = {
-        'form': form
-    }
-    return render(request, 'depart_add.html', context)
+    form = DepartmentForm(data=request.POST)       
+    if form.is_valid():
+        form.save()
+    return redirect('/depart/list/')
 
 def depart_delete(request):
     pass
