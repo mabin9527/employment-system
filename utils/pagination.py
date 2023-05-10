cfbb39a2b455ae7f78f3cb8d1dce82be50890333
mabin9527 <ma.bin9527000@gmail.com>
2023-05-10 19:25:25 +0000

    Set up ultils file folder on top level and create pagination class inside this folder. Explain each parameters

---
+++ utils/pagination.py
@@ -0,0 +1,14 @@
+class Pagination(object):
+    """
+    Build reusable pagination class
+    """
+
+    def __init__(self, request, queryset, page_size=10, page_param='page', plus=5):
+        """
+        :param request: The requested object
+        :param page_size: The amount of data displayed per page
+        :param page_param: The page parameters
+        :param plus: The number of pages is moved back or moved forward from current page
+        """
+
+        
