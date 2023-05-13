79ff7be21df596e6b08dfba49ba91e65b3cf6635
mabin9527 <ma.bin9527000@gmail.com>
2023-05-13 09:20:27 +0000

    Create js folder. Move the common js code from admin_list, employee_list and depart_list to static/js

---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+
+  var DELETE_ID
+  $(function() {
+    bindBtnDeleteEvent();
+    bindBtnConfirmDeleteEvent();
+  })
+
+  function bindBtnDeleteEvent() {
+    $('.btn-delete').click(function() {
+      $('#myModal').modal('show');
+
+      DELETE_ID = $(this).attr('uid');
+    });
+  }
+
+  function bindBtnConfirmDeleteEvent() {
+    $('.btnConfirmDelete').click(function() {
+      $.ajax({
+        url: '/admin/' + DELETE_ID + '/delete/',
+        type: 'GET',
+        dataType: 'JSON',
+        success: function (res) {
+          if (res.status) {
+            $('#myModal').modal('hide');
+            $("tr[uid='"+DELETE_ID+"']").remove();
+            DELETE_ID = 0;
+          }else{
+            alert(res.error);
+          }
+        }
+      })
+    });
+  }
