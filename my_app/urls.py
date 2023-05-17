e9905d9a9cab6453ab13deda1a0bc48cf53c2b00
mabin9527 <ma.bin9527000@gmail.com>
2023-05-17 10:06:31 +0000

    Update urls code

---
+++ my_app/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from my_app import views
+
+urlpatterns = [
+    path('depart/list/', views.depart_list),
+    path('depart/add/', views.depart_add),
+    path('depart/delete/', views.depart_delete),
+    path('depart/<int:nid>/edit/', views.depart_edit),
+    path('employee/list/', views.employee_list),
+    path('employee/add/', views.employee_add),
+    path('employee/<int:nid>/edit/', views.employee_edit),
+    path('employee/<int:nid>/delete/', views.employee_delete),
+    path('admin/list/', views.admin_list),
+    path('admin/add/', views.admin_add),
+    path('admin/<int:nid>/edit/', views.admin_edit),
+    path('admin/<int:nid>/delete/', views.admin_delete),
+    path('login/', views.login),
+    path('logout/', views.logout),
+]
